812fb8c620516325e81f350258c63d0123bf6a26
saiful.ramelan <saiful.ramelan@gmail.com>
2018-10-16 10:27:01 +0700

    belajar array

---
+++ array.php
@@ -0,0 +1,20 @@
+<?php
+
+$variable1 = "Hello ";
+$variable2 = "Mabes TNI";
+echo $variable1; echo $variable2;
+
+echo '<br>';
+$variable = array("Hello ", "Mabes TNI");
+echo $variable [0];
+echo $variable [1];
+
+?>
+
+<?php
+$variable=array("web"=>"mabestni.com","email"=>"pusinfolahta@mabestni.com");
+echo '<br>';
+echo "Kunjungi Mabes TNI, 
+'<br>'website: " .$variable['web'].",
+'<br>'email:" .$variable['email'];
+?>
